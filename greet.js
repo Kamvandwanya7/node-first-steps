9d3ab1ff4148f48b91a0ad51935bd14e7361e5ee
Kamva <kayndwanya24@gmail.com>
2022-07-04 16:47:18 +0200

    initial commit

---
+++ greet.js
@@ -0,0 +1,10 @@
+// var msg= "Xola"
+// function greet(){
+// return "Hello, " + msg;
+// }
+// console.log(msg)
+function greet (name){
+    
+    return "Hello, " + name
+}
+export default greet
---
+++ greet_xola.js
@@ -0,0 +1,38 @@
+
+
+
+import chalk from 'chalk';
+//import the greet module that is in the current folder
+import greet from './greet.js';
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
+
+
+
+
+
+import figlet from 'figlet';
+
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
+
+
+
+
+
+
+
+
+
+// const error = chalk.bold.red;
+// const warning = chalk.hex('#FFA500'); // Orange color
+
+// console.log(error('Error!'));
+// console.log(warning('Warning!'));
