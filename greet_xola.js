488d595bad8f7420eeee2713c2daee6d1ffd76dd
Kamva <kayndwanya24@gmail.com>
2022-07-05 11:24:48 +0200

    committed

--- greet_xola.js
+++ greet_xola.js
@@ -11,20 +11,6 @@ console.log(styledMessage)
 
 
 
-var figlet = require('figlet');
-import figlet from 'figlet'
-
-figlet('Hello World!!', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
-});
-
-
-
 
 
 
